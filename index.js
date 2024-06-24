dd6bcb0043dd92e8e15a2d636356f19480dd9460
Philip osir Otieno <philiposir2@gmail.com>
2024-06-24 18:39:20 +0300

    add the JS node

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+const mainElement = document.querySelector('main#main')
+mainElement.remove("main");
+
+const newHeader = document.createElement("h1");
+
+newHeader.id = "victory";
+
+const myName = "PhilipOsir"
+newHeader.textContent = `${myName} is the champion`;
+document.body.appendChild(newHeader)
